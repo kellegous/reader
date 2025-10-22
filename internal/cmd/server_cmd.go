5987e33be75a4a47df5555ba951e89397cb5d40d
Kelly Norton <kellegous@gmail.com>
2025-10-22 08:55:13 -0400

    moving to cobra

--- internal/cmd/server_cmd.go
+++ internal/cmd/server_cmd.go
@@ -1,12 +1,23 @@
 package cmd
 
 import (
+	"context"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+
 	"github.com/kellegous/glue/logging"
 	"github.com/kellegous/poop"
 	"github.com/kellegous/reader/internal/config"
+	"github.com/kellegous/reader/internal/miniflux"
+	"github.com/kellegous/reader/internal/postgres"
+	"github.com/kellegous/reader/internal/web"
 	"github.com/spf13/cobra"
 	"go.uber.org/zap"
 )
+
+const backendAddr = "127.0.0.1:9090"
 
 func serverCmd() *cobra.Command {
 	var flags struct {
@@ -35,5 +46,95 @@ func runServer(cmd *cobra.Command, configFile string, debug bool) error {
 		return poop.Chain(err)
 	}
 
+	ctx, done := signal.NotifyContext(cmd.Context(), os.Interrupt)
+	defer done()
+
+	lg := logging.L(cmd.Context())
+
+	lg.Info("starting reader",
+		zap.String("postgress.data-dir", cfg.Postgres.DataDir))
+
+	ch := make(chan error, 1)
+
+	l, err := net.Listen("tcp", cfg.Web.Addr)
+	if err != nil {
+		return poop.Chain(err)
+	}
+	defer l.Close()
+
+	go func() {
+		ch <- web.Serve(ctx, l, "http://"+backendAddr)
+	}()
+
+	pg, err := ensurePostgresReady(ctx, &cfg.Postgres)
+	if err != nil {
+		return poop.Chain(err)
+	}
+	defer pg.Stop(context.Background())
+
+	// TODO(knorton): get pid from postgres
+	lg.Info("postgres started", zap.Int("pid", 0))
+
+	mf, err := startMiniflux(
+		ctx,
+		fmt.Sprintf("https://%s/", cfg.Web.Hostname),
+		&cfg,
+		debug)
+	if err != nil {
+		return poop.Chain(err)
+	}
+	defer mf.Stop()
+
+	select {
+	case <-ctx.Done():
+	case err := <-ch:
+		if err != nil {
+			return poop.Chain(err)
+		}
+	}
+
 	return nil
+}
+
+func ensurePostgresReady(
+	ctx context.Context,
+	cfg *config.Postgres,
+) (*postgres.Server, error) {
+	s, err := postgres.Start(ctx, cfg.DataDir)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := s.EnsureDatabase(
+		ctx,
+		cfg.Database,
+		cfg.Username,
+		cfg.Password,
+	); err != nil {
+		s.Stop(ctx)
+		return nil, err
+	}
+
+	return s, nil
+}
+
+func startMiniflux(
+	ctx context.Context,
+	baseURL string,
+	cfg *config.Info,
+	debug bool,
+) (*miniflux.Server, error) {
+	return miniflux.Start(
+		ctx,
+		miniflux.WithAdmin(
+			cfg.Miniflux.AdminUsername,
+			cfg.Miniflux.AdminPassword),
+		miniflux.WithDatabase(
+			cfg.Postgres.Database,
+			cfg.Postgres.Username, cfg.Postgres.Password),
+		miniflux.WithRunMigrations(true),
+		miniflux.WithListenAddress(backendAddr),
+		miniflux.WithBaseURL(baseURL),
+		miniflux.WithDebugLogging(debug),
+	)
 }
