16e71850a13a1006a59d432077a0ac54816b8cc9
Kelly Norton <kellegous@gmail.com>
2025-10-26 12:05:10 -0400

    refactor mf options

--- internal/miniflux/option.go
+++ internal/miniflux/option.go
@@ -5,18 +5,68 @@ import (
 	"net"
 
 	"github.com/kellegous/poop"
-	"miniflux.app/v2/client"
 )
 
-type Option func(*Server) error
+type Option func(*Options) error
+
+type Options struct {
+	databaseURL           string
+	externalURL           string
+	internalURL           string
+	listenAddr            string
+	admin                 *login
+	runMigrations         bool
+	authProxyHeader       string
+	authProxyUserCreation bool
+	debugLogging          bool
+}
+
+type login struct {
+	username string
+	password string
+}
+
+func (o *Options) env() []string {
+	var env []string
+	if o.databaseURL != "" {
+		env = append(env, "DATABASE_URL="+o.databaseURL)
+	}
+	if o.listenAddr != "" {
+		env = append(env, "LISTEN_ADDR="+o.listenAddr)
+	}
+	if o.externalURL != "" {
+		env = append(env, "BASE_URL="+o.externalURL)
+	}
+	if o.listenAddr != "" {
+		env = append(env, "LISTEN_ADDR="+o.listenAddr)
+	}
+	if a := o.admin; a != nil {
+		env = append(env, "CREATE_ADMIN=1")
+		env = append(env, "ADMIN_USERNAME="+a.username)
+		env = append(env, "ADMIN_PASSWORD="+a.password)
+	}
+	if o.runMigrations {
+		env = append(env, "RUN_MIGRATIONS=1")
+	}
+	if o.authProxyHeader != "" {
+		env = append(env, "AUTH_PROXY_HEADER="+o.authProxyHeader)
+	}
+	if o.authProxyUserCreation {
+		env = append(env, "AUTH_PROXY_USER_CREATION=1")
+	}
+	if o.debugLogging {
+		env = append(env, "DEBUG=1")
+	}
+	return env
+}
 
 func WithDatabase(
 	name string,
 	username string,
 	password string,
 ) Option {
-	return func(s *Server) error {
-		s.env["DATABASE_URL"] = fmt.Sprintf(
+	return func(o *Options) error {
+		o.databaseURL = fmt.Sprintf(
 			"user=%s password=%s dbname=%s sslmode=disable",
 			username,
 			password,
@@ -41,70 +91,58 @@ func toBaseURL(addr string) (string, error) {
 }
 
 func WithListenAddress(addr string) Option {
-	return func(s *Server) error {
+	return func(o *Options) error {
 		url, err := toBaseURL(addr)
 		if err != nil {
 			return poop.Chain(err)
 		}
-		s.env["LISTEN_ADDR"] = addr
-		s.baseURL = url
+		o.listenAddr = addr
+		o.internalURL = url
 		return nil
 	}
 }
 
 func WithBaseURL(url string) Option {
-	return func(s *Server) error {
-		s.env["BASE_URL"] = url
+	return func(o *Options) error {
+		o.externalURL = url
 		return nil
 	}
 }
 
 func WithAdmin(username string, password string) Option {
-	return func(s *Server) error {
-		s.env["CREATE_ADMIN"] = "1"
-		s.env["ADMIN_USERNAME"] = username
-		s.env["ADMIN_PASSWORD"] = password
-
-		s.opts = []client.Option{
-			client.WithCredentials(username, password),
+	return func(o *Options) error {
+		o.admin = &login{
+			username: username,
+			password: password,
 		}
-
 		return nil
 	}
 }
 
 func WithRunMigrations(v bool) Option {
-	return func(s *Server) error {
-		if v {
-			s.env["RUN_MIGRATIONS"] = "1"
-		} else {
-			delete(s.env, "RUN_MIGRATIONS")
-		}
+	return func(o *Options) error {
+		o.runMigrations = v
 		return nil
 	}
 }
 
 func WithAuthProxyHeader(header string) Option {
-	return func(s *Server) error {
-		s.env["AUTH_PROXY_HEADER"] = header
+	return func(o *Options) error {
+		o.authProxyHeader = header
 		return nil
 	}
 }
 
 func WithAuthProxyUserCreation(v bool) Option {
-	return func(s *Server) error {
-		s.env["AUTH_PROXY_USER_CREATION"] = "1"
+	return func(o *Options) error {
+		o.authProxyUserCreation = v
 		return nil
 	}
 }
 
 func WithDebugLogging(v bool) Option {
-	return func(s *Server) error {
-		if v {
-			s.env["DEBUG"] = "1"
-		} else {
-			delete(s.env, "DEBUG")
-		}
+	return func(o *Options) error {
+		o.debugLogging = v
 		return nil
 	}
 }
--- internal/miniflux/server.go
+++ internal/miniflux/server.go
@@ -9,10 +9,8 @@ import (
 )
 
 type Server struct {
-	env     map[string]string
-	proc    *os.Process
-	baseURL string
-	opts    []client.Option
+	proc *os.Process
+	opts Options
 }
 
 func (s *Server) Stop() error {
@@ -20,28 +18,29 @@ func (s *Server) Stop() error {
 }
 
 func (s *Server) Client(opts ...client.Option) *client.Client {
+	if a := s.opts.admin; a != nil {
+		opts = append(opts, client.WithCredentials(a.username, a.password))
+	}
+
 	return client.NewClientWithOptions(
-		s.baseURL,
-		append(s.opts, opts...)...)
+		s.opts.externalURL,
+		opts...)
 }
 
 func (s *Server) BaseURL() string {
-	return s.baseURL
+	return s.opts.internalURL
 }
 
 func Start(ctx context.Context, opts ...Option) (*Server, error) {
-	s := &Server{
-		env: map[string]string{},
-	}
-
+	s := &Server{}
 	for _, opt := range opts {
-		if err := opt(s); err != nil {
+		if err := opt(&s.opts); err != nil {
 			return nil, err
 		}
 	}
 
 	c := exec.CommandContext(ctx, "miniflux")
-	c.Env = s.getEnv()
+	c.Env = s.opts.env()
 	c.Stdout = os.Stdout
 	c.Stderr = os.Stderr
 	if err := c.Start(); err != nil {
@@ -51,12 +50,4 @@ func Start(ctx context.Context, opts ...Option) (*Server, error) {
 	s.proc = c.Process
 
 	return s, nil
-}
-
-func (s *Server) getEnv() []string {
-	env := make([]string, 0, len(s.env))
-	for k, v := range s.env {
-		env = append(env, k+"="+v)
-	}
-	return env
 }
