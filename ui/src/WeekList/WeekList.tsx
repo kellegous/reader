f427e7e0c491ca4b7815b7a238b274c8ccf2c3c6
Kelly Norton <kellegous@gmail.com>
2025-10-27 21:27:41 -0400

    skeleton week list

--- ui/src/WeekList/WeekList.tsx
+++ ui/src/WeekList/WeekList.tsx
@@ -3,7 +3,20 @@ import { useReaderData } from "../ReaderDataContext";
 export const WeekList = () => {
   const { weeks } = useReaderData();
 
-  return weeks.map(({ week }) => (
-    <div key={week.startsAt.getTime()}>{week.toString()}</div>
+  return weeks.map(({ week, entries }) => (
+    <div key={week.startsAt.getTime()}>
+      <h2>
+        {week.toString()} ({entries.length})
+      </h2>
+      <ul>
+        {entries.map((entry) => (
+          <li key={entry.id}>
+            <a href={entry.url} target="_blank" rel="noopener noreferrer">
+              {entry.feed?.title} / {entry.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
   ));
 };
