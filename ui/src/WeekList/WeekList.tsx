c0abd5a362fe8b885a634b29ff26d4890a8044af
Kelly Norton <kellegous@gmail.com>
2025-10-28 18:51:39 -0400

    add to week list

--- ui/src/WeekList/WeekList.tsx
+++ ui/src/WeekList/WeekList.tsx
@@ -1,22 +1,27 @@
 import { useReaderData } from "../ReaderDataContext";
+import styles from "./WeekList.module.scss";
 
 export const WeekList = () => {
   const { weeks } = useReaderData();
 
-  return weeks.map(({ week, entries }) => (
-    <div key={week.startsAt.getTime()}>
-      <h2>
-        {week.toString()} ({entries.length})
-      </h2>
-      <ul>
-        {entries.map((entry) => (
-          <li key={entry.id}>
-            <a href={entry.url} target="_blank" rel="noopener noreferrer">
-              {entry.feed?.title} / {entry.title}
-            </a>
-          </li>
-        ))}
-      </ul>
+  return (
+    <div className={styles.root}>
+      {weeks.map(({ week, entries }) => (
+        <div key={week.startsAt.getTime()}>
+          <h2>
+            {week.toString()} ({entries.length})
+          </h2>
+          <ul>
+            {entries.map((entry) => (
+              <li key={entry.id}>
+                <a href={entry.url} target="_blank" rel="noopener noreferrer">
+                  {entry.feed?.title} / {entry.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
-  ));
+  );
 };
