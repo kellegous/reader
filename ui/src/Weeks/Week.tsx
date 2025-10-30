f62ddd1232043da311d3eced89518c495d1ae6a8
Kelly Norton <kellegous@gmail.com>
2025-10-29 21:27:49 -0400

    styles

--- ui/src/Weeks/Week.tsx
+++ ui/src/Weeks/Week.tsx
@@ -1,6 +1,7 @@
 import * as time from "../time";
 import * as proto from "../gen/reader";
 import { Feed } from "./Feed";
+import styles from "./Week.module.scss";
 
 const dayFormat = new Intl.DateTimeFormat("en-US", {
   month: "short",
@@ -17,9 +18,9 @@ export interface WeekProps {
 
 export const Week = ({ week, feeds }: WeekProps) => {
   return (
-    <div>
-      <h2>{formatWeek(week)}</h2>
-      <div>
+    <div className={styles.root}>
+      <div className={styles.title}>{formatWeek(week)}</div>
+      <div className={styles.feeds}>
         {feeds.map(({ feed, entries }) => (
           <Feed key={feed.id} feed={feed} entries={entries} />
         ))}
